44769b1dfcfb041fe327bba20416898fc881af3d
Zach Riggle <zach@riggle.me>
2013-03-24 15:48:33 -0400

    More js

--- js/app.js
+++
@@ -1,7 +0,0 @@
-App = Ember.Application.create({});
-
-App.IndexRoute = Ember.Route.extend({
-  setupController: function(controller) {
-    controller.set('content', ['red', 'yellow', 'blue']);
-  }
-});
