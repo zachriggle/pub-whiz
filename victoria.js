408567f7da88831d703a525be749ac6175dd5026
Zach Riggle <zach@riggle.me>
2013-03-27 06:33:28 -0400

    Added VGP

---
+++ victoria.js
@@ -0,0 +1,4 @@
+
+$(function () {
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/0Ysem", "description": "Restaurant and bar serving modern British fare, brunch, lunch & dinner with wide selection of draft beers. Late night menu and private dining.", "url": "http://www.victoriagastropub.com", "name": "Victoria Gastropub", "beers": [180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203], "id": 4, "deatils": "A place with a warm, inviting atmosphere that serves sophisticated & innovative creations on classic pub fare with an eclectic beer & wine list."});
+})
---
+++ victoria.py
@@ -0,0 +1,30 @@
+import urllib2
+from bs4 import BeautifulSoup
+from beercommon import SearchForBeers, DumpBarToFixtures
+
+#
+# VGP is stupid and paginates their beer
+#
+
+
+beersNamesUnicode = []
+
+for page in [1,2,3]:
+  url  = "http://victoriagastropub.com/?menutype=beer&paged=%s" % page
+  data = urllib2.urlopen(url).read()
+  soup = BeautifulSoup(data)
+
+  beersNamesUnicode += soup.findAll('h2', 'menu-title')
+
+beerNames = [i.contents[1].encode('utf-8') for i in beersNamesUnicode]
+
+results = SearchForBeers(beerNames)
+
+DumpBarToFixtures("victoria.js", beerNames, {
+  'id': 4,
+  'name': "Victoria Gastropub",
+  'description': "Restaurant and bar serving modern British fare, brunch, lunch & dinner with wide selection of draft beers. Late night menu and private dining.",
+  'deatils': "A place with a warm, inviting atmosphere that serves sophisticated & innovative creations on classic pub fare with an eclectic beer & wine list.",
+  'url': "http://www.victoriagastropub.com",
+  'map': "http://goo.gl/maps/0Ysem",
+})
