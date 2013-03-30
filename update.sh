806e15a5cf7ca755b2ed59649221c1a41128750d
Zach Riggle <zach@riggle.me>
2013-03-29 21:21:09 -0400

    Add update script

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+git stash save
+git checkout master
+python maxs.py
+python frisco.py 
+python victoria.py
+python alehouse.py
+jade index.html
+git add -u
+git commit -m "Updated on $(date)"
+git push origin master
+git push production master
+git checkout gh-pages
+git merge master -m "merged on $(date)"
+git push origin gh-pages
+git checkout master
+git stash apply
