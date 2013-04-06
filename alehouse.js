673aa57b723cec6c22ead3d30da0d802588798aa
Zach Riggle <zach@riggle.me>
2013-04-06 16:37:33 -0400

    Updated on Sat Apr  6 16:37:33 EDT 2013

--- alehouse.js
+++ alehouse.js
@@ -1,4 +1,4 @@
 
 $(function () {
-App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/IlqCL", "description": "The Ale House Columbia is Howard County's destination for craft beers, food and nightlife. Come see what makes us more than the typical pub or sports bar.", "url": "http://www.thealehousecolumbia.com", "beers": [24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 31, 35, 36, 37, 38, 39, 40, 41, 316, 42, 43, 44, 45, 46, 47, 48, 49], "details": "", "id": 3, "name": "The Ale House"});
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/IlqCL", "description": "The Ale House Columbia is Howard County's destination for craft beers, food and nightlife. Come see what makes us more than the typical pub or sports bar.", "url": "http://www.thealehousecolumbia.com", "beers": [335, 26, 336, 28, 29, 337, 30, 338, 339, 31, 32, 33, 31, 340, 341, 342, 343, 344, 36, 345, 346, 347, 348, 349, 35, 36, 40, 350, 351, 316, 44, 352, 46, 47], "details": "", "id": 3, "name": "The Ale House"});
 })
