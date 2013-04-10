0568b5ca96b44867aca32f1ddb4dab385023249d
Zach Riggle <zach@riggle.me>
2013-04-09 22:37:10 -0400

    Updated on Tue Apr  9 22:37:10 EDT 2013

--- victoria.js
+++ victoria.js
@@ -1,4 +1,4 @@
 
 $(function () {
-App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/0Ysem", "description": "Restaurant and bar serving modern British fare, brunch, lunch & dinner with wide selection of draft beers. Late night menu and private dining.", "url": "http://www.victoriagastropub.com", "name": "Victoria Gastropub", "beers": [273, 274, 238, 275, 276, 262, 263, 264, 1, 2, 4, 7, 8, 10, 13, 15, 16, 17, 19, 20, 21, 22, 23], "id": 4, "deatils": "A place with a warm, inviting atmosphere that serves sophisticated & innovative creations on classic pub fare with an eclectic beer & wine list."});
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/0Ysem", "description": "Restaurant and bar serving modern British fare, brunch, lunch & dinner with wide selection of draft beers. Late night menu and private dining.", "url": "http://www.victoriagastropub.com", "name": "Victoria Gastropub", "beers": [363, 364, 275, 365, 366, 367, 179, 368, 369, 370, 371, 372, 373, 262, 4, 7, 13, 16, 17, 20, 21, 22, 23], "id": 4, "deatils": "A place with a warm, inviting atmosphere that serves sophisticated & innovative creations on classic pub fare with an eclectic beer & wine list."});
 })
