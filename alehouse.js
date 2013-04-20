f854841b233ff050931c6ee2cf21336b35687248
Zach Riggle <zach@riggle.me>
2013-04-19 21:05:34 -0400

    Updated on Fri Apr 19 21:05:34 EDT 2013

--- alehouse.js
+++ alehouse.js
@@ -1,4 +1,4 @@
 
 $(function () {
-App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/IlqCL", "description": "The Ale House Columbia is Howard County's destination for craft beers, food and nightlife. Come see what makes us more than the typical pub or sports bar.", "url": "http://www.thealehousecolumbia.com", "beers": [548, 25, 26, 336, 28, 337, 30, 338, 548, 28, 549, 550, 340, 342, 343, 344, 36, 40, 351, 316, 551, 552, 42, 44, 553, 554, 412, 413, 47, 555, 352, 158], "details": "", "id": 3, "name": "The Ale House"});
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/IlqCL", "description": "The Ale House Columbia is Howard County's destination for craft beers, food and nightlife. Come see what makes us more than the typical pub or sports bar.", "url": "http://www.thealehousecolumbia.com", "beers": [548, 25, 26, 336, 28, 337, 338, 548, 28, 559, 560, 561, 562, 563, 550, 340, 342, 343, 344, 36, 40, 351, 316, 551, 44, 554, 564, 413, 47, 555, 352, 158], "details": "", "id": 3, "name": "The Ale House"});
 })
--- kloby.js
+++ kloby.js
@@ -1,4 +1,4 @@
 
 $(function () {
-App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/QJnV4", "description": "", "url": "http://www.klobysbbq.com/", "name": "Kloby's Smokehouse", "beers": [426, 427, 428, 429, 430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 386, 443, 405, 401, 444, 445, 388, 394, 396, 446, 447, 400, 158, 448, 449], "id": 5, "deatils": ""});
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/QJnV4", "description": "", "url": "http://www.klobysbbq.com/", "name": "Kloby's Smokehouse", "beers": [565, 566, 567, 231, 568, 569, 570, 571, 426, 427, 428, 429, 431, 432, 572, 434, 435, 436, 437, 438, 439, 384, 219, 442, 386, 443, 573, 574, 444, 158, 448, 449], "id": 5, "deatils": ""});
 })
