334e285da9d7bb0a46dae60226ae360ed80ffd62
Zach Riggle <zach@riggle.me>
2013-04-22 21:58:35 -0400

    Updated on Mon Apr 22 21:58:35 EDT 2013

--- kloby.js
+++ kloby.js
@@ -1,4 +1,4 @@
 
 $(function () {
-App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/QJnV4", "description": "", "url": "http://www.klobysbbq.com/", "name": "Kloby's Smokehouse", "beers": [565, 566, 567, 231, 568, 569, 570, 571, 426, 427, 428, 429, 431, 432, 572, 434, 435, 436, 437, 438, 439, 384, 219, 442, 386, 443, 573, 574, 444, 158, 448, 449], "id": 5, "deatils": ""});
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/QJnV4", "description": "", "url": "http://www.klobysbbq.com/", "name": "Kloby's Smokehouse", "beers": [601, 565, 566, 602, 231, 568, 569, 570, 571, 426, 603, 429, 604, 432, 572, 434, 435, 436, 437, 438, 439, 384, 219, 442, 386, 605, 573, 574, 444, 158, 606, 449], "id": 5, "deatils": ""});
 })
