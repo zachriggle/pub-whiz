d86b6ffcd9bf520e9f350e6e9878e2aef2173f50
Zach Riggle <zach@riggle.me>
2013-04-26 18:01:36 -0400

    Updated on Fri Apr 26 18:01:36 EDT 2013

--- kloby.js
+++ kloby.js
@@ -1,4 +1,4 @@
 
 $(function () {
-App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/QJnV4", "description": "", "url": "http://www.klobysbbq.com/", "name": "Kloby's Smokehouse", "beers": [601, 565, 566, 602, 231, 568, 569, 570, 571, 426, 603, 429, 604, 432, 572, 434, 435, 436, 437, 438, 439, 384, 219, 442, 386, 605, 573, 574, 444, 158, 606, 449], "id": 5, "deatils": ""});
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/QJnV4", "description": "", "url": "http://www.klobysbbq.com/", "name": "Kloby's Smokehouse", "beers": [395, 666, 667, 668, 669, 601, 565, 566, 567, 231, 670, 570, 571, 426, 671, 429, 672, 432, 434, 435, 437, 438, 673, 384, 219, 442, 386, 443, 574, 444, 158, 449], "id": 5, "deatils": ""});
 })
--- victoria.js
+++ victoria.js
@@ -1,4 +1,4 @@
 
 $(function () {
-App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/0Ysem", "description": "Restaurant and bar serving modern British fare, brunch, lunch & dinner with wide selection of draft beers. Late night menu and private dining.", "url": "http://www.victoriagastropub.com", "name": "Victoria Gastropub", "beers": [590, 591, 592, 593, 594, 382, 595, 9, 596, 597, 598, 599, 600, 514, 515, 173, 519, 521, 275, 20, 21, 22, 23], "id": 4, "deatils": "A place with a warm, inviting atmosphere that serves sophisticated & innovative creations on classic pub fare with an eclectic beer & wine list."});
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/0Ysem", "description": "Restaurant and bar serving modern British fare, brunch, lunch & dinner with wide selection of draft beers. Late night menu and private dining.", "url": "http://www.victoriagastropub.com", "name": "Victoria Gastropub", "beers": [657, 658, 659, 660, 661, 662, 663, 590, 591, 593, 594, 595, 9, 597, 599, 519, 521, 275, 20, 21, 22, 23], "id": 4, "deatils": "A place with a warm, inviting atmosphere that serves sophisticated & innovative creations on classic pub fare with an eclectic beer & wine list."});
 })
