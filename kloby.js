d7c13130d164367b75ac8ee732359e82fb7f3639
Zach Riggle <zach@riggle.me>
2013-05-18 20:11:51 -0400

    kloby

---
+++ kloby.js
@@ -0,0 +1,4 @@
+
+$(function () {
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/QJnV4", "description": "", "url": "http://www.klobysbbq.com/", "name": "Kloby's Smokehouse", "beers": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32], "id": 5, "deatils": ""});
+})
