0dfaef3ea2d97cd04ca9c7f219812c50fa678fb2
Zach Riggle <zachriggle@gmail.com>
2013-05-28 08:12:41 +0400

    Updated on Tue May 28 08:12:41 MSK 2013

--- alehouse.js
+++ alehouse.js
@@ -1,4 +1,4 @@
 
 $(function () {
-App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/IlqCL", "description": "The Ale House Columbia is Howard County's destination for craft beers, food and nightlife. Come see what makes us more than the typical pub or sports bar.", "url": "http://www.thealehousecolumbia.com", "beers": [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 31], "details": "", "id": 3, "name": "The Ale House"});
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/IlqCL", "description": "The Ale House Columbia is Howard County's destination for craft beers, food and nightlife. Come see what makes us more than the typical pub or sports bar.", "url": "http://www.thealehousecolumbia.com", "beers": [349, 350, 33, 34, 35, 36, 37, 38, 351, 41, 352, 353, 354, 355, 356, 357, 358, 45, 46, 47, 49, 51, 52, 54, 55, 56, 353, 359, 360, 60, 361], "details": "", "id": 3, "name": "The Ale House"});
 })
--- arundel.js
+++ arundel.js
@@ -1,4 +1,4 @@
 
 $(function () {
-App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/gMS7W", "description": "The #1 craft brewery in Maryland (ratebeer.com), DuClaw Brewing Company offers award winning handcrafted beers with tastes and flavors as provocative as their names.", "url": "http://www.duclaw.com", "beers": [188, 189, 190, 191, 192, 193, 194, 195, 196], "details": "", "id": 7, "name": "DuClaw"});
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/gMS7W", "description": "The #1 craft brewery in Maryland (ratebeer.com), DuClaw Brewing Company offers award winning handcrafted beers with tastes and flavors as provocative as their names.", "url": "http://www.duclaw.com", "beers": [375, 188, 189, 376, 190, 191, 192, 377, 193, 195, 196], "details": "", "id": 7, "name": "DuClaw"});
 })
