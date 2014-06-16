175d1547b46b52892a9841fb2a6e42416ac9536f
Zach Riggle <zach@riggle.me>
2014-06-16 19:24:01 -0400

    Updated

---
+++ frisco.js
@@ -0,0 +1,4 @@
+
+$(function () {
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/kKMNp", "description": "Delicious regional American food and Columbia's largest selection of micro-brews on tap.", "url": "http://www.friscogrille.com", "beers": [213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262], "details": "A restaurant serving Southwestern cuisine, Frisco's real draw is its outstanding beer program, with 50 beers on draft, including two cask ales, and brews its own ales on site. (Look for the Push Craft Brewing taps.)", "id": 2, "name": "Frisco Grille"});
+})
---
+++ kloby.js
@@ -0,0 +1,4 @@
+
+$(function () {
+App.Bar.FIXTURES.push({"map": "http://goo.gl/maps/QJnV4", "description": "", "url": "http://www.klobysbbq.com/", "name": "Kloby's Smokehouse", "beers": [263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282], "id": 5, "deatils": ""});
+})
