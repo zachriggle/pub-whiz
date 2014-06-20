ce61a0ee8ce8a937bb689ed7cc3c9e4f4051510a
Zach Riggle <zach@riggle.me>
2014-06-20 19:29:42 -0400

    Update script

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git stash save
-git checkout master
+git checkout gh-pages
 echo "Maxs" && python maxs.py
 echo "Frisco" && python frisco.py
 echo "VGP" && python victoria.py
@@ -11,10 +11,5 @@ echo "Arundel" && python arundel.py
 jade index.jade
 git add -u
 git commit -m "Updated on $(date)"
-git push origin master
-# git push production master
-git checkout gh-pages
-git merge master -m "merged on $(date)"
 git push origin gh-pages
-git checkout master
 git stash apply
